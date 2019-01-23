47b76e29cb2a4ee0eb989d579eb6ac162793d2c4
Nossair <n0ss4@protonmail.com>
2019-01-23 17:10:47 +0100

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Nossair\OneDrive\Documentos\2 DAW\MiniProyecto\trobafeina\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
