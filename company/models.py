04940231f442c20d5c45398946332c19151f05b9
Nossair <n0ss4@protonmail.com>
2019-01-24 23:50:08 +0100

    Pagina por defecto con un mensaje de la creación de la web. Modficiado el nombre de las variables con el verbose_name. Implementado el accounts/login. Y mucho más...

--- company/models.py
+++ company/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Company(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cif = models.CharField(max_length=9, blank=False,)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Nom del usuari")
+    cif = models.CharField(max_length=9, blank=False,verbose_name="CIF", primary_key=True)
     sector = models.CharField(max_length=500, blank=False)
-    descripcio = models.CharField(max_length=500, blank=False)
-    nomResponsable = models.CharField(max_length=500, blank=False)
-    cognomResponsable = models.CharField(max_length=500, blank=False)
+    descripcio = models.TextField(max_length=500, blank=False,verbose_name="Descripció")
+    nomResponsable = models.CharField(max_length=500, blank=False, verbose_name="Nom del responsable")
+    cognomResponsable = models.CharField(max_length=500, blank=False, verbose_name="Cognom del responsable")
+
+    def __str__(self):
+        return self.cif
--- student/models.py
+++ student/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=500, blank=True)
-    surname = models.CharField(max_length=500, blank=True)
-    dni = models.CharField(max_length=9, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Nom del usuari")
+    name = models.CharField(max_length=500, blank=True, verbose_name="Nom")
+    surname = models.CharField(max_length=500, blank=True, verbose_name="Cognoms")
+    dni = models.CharField(max_length=9, blank=True, verbose_name="DNI", primary_key=True)
+
+    def __str__(self):
+        return self.name+" "+self.surname
