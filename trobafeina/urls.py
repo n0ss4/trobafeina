d8cc49a81259bd7ba1f9ed36894de15ffda11688
Nossair <n0ss4@protonmail.com>
2019-01-25 16:49:55 +0100

    Modificación del diseño de la pàgina web.

--- trobafeina/urls.py
+++ trobafeina/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url('',views.index)
+    url(r'^', include('django.contrib.auth.urls')),
+    path('', views.index)
 ]
