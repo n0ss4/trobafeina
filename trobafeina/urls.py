b7c782f8ba9e3eded4700d55d1e7ee0488ef76dc
Nossair <n0ss4@protonmail.com>
2019-01-28 18:03:51 +0100

    Arreglar bugs que contenia la app.

--- trobafeina/urls.py
+++ trobafeina/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('django.contrib.auth.urls')),
-    path('', views.index)
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    path('', views.index),
 ]
