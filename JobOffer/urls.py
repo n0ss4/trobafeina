1d14f2edaf3493b60d133dab9444a0e70350ef7d
Nossair <n0ss4@protonmail.com>
2019-01-29 16:08:15 +0100

    ertrte
    erter

--- JobOffer/urls.py
+++ JobOffer/urls.py
@@ -4,10 +4,7 @@ app_name = 'joboffer'
 urlpatterns = [
     path('', views.index, name='index'),
     path('registre_estudiant/', include('student.urls')),
-<<<<<<< HEAD
     path('registre_empresa/', include('company.urls')),
     path('formularis/', views.formularis, name='formularis'),
-
-=======
->>>>>>> aa12a5994c8063dfc4a80bbd8d79e0345d42eb36
+    path('registre_empresa/', include('company.urls'))
 ]
