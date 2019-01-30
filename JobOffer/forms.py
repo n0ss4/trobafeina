a3e84a77310ddb6a19094a013ebbbda563fb4ca5
Nossair <n0ss4@protonmail.com>
2019-01-30 16:43:48 +0100

    Disseny del registre

--- JobOffer/forms.py
+++ JobOffer/forms.py
@@ -1,12 +1,11 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Joboffer
+
+
 class PubJobOffer(ModelForm):
+
     class Meta:
         model = Joboffer
-        fields = ["requirements","nom","experience","minimum_requirements","description","numero_de_vacants","salari"]#Seran els que es mostraran al formulari
-        widgets = {
-            'nom': forms.TextInput(attrs={'placeholder':'nom'}),
-            'requirements': forms.Textarea(attrs={'placeholder': 'nom'}),
-
-        }
+        fields = ["requirements", "nom", "experience", "minimum_requirements", "description", "numero_de_vacants", "salari"]#Seran els que es mostraran al formulari
+        widgets = { 'nom': forms.TextInput(attrs={'placeholder':'nom'}), 'requirements': forms.Textarea(attrs={'placeholder': 'nom'}),}
